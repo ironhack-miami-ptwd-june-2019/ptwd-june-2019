e89275e039a84c1b940b79cb029804ff202796aa
sandrabosk <sandraboskovic@hotmail.com>
2019-08-03 17:09:56 -0400

    intro to node - class review

---
+++ module2/lab-intro-node-class-review/index.js
@@ -0,0 +1,44 @@
+class SortedList {
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length ++;
+    }
+    get(pos) {
+      if(!this.length || pos > this.length){
+        throw Error("OutOfBounds");
+      }
+      return this.items[pos];
+    }
+    max() {
+      if(!this.length){
+        throw Error ("EmptySortedList");
+      }
+      return Math.max(...this.items);
+      // return this.items[this.length-1];
+    }
+    min() {
+      if(!this.length){
+        throw Error ("EmptySortedList");
+      }
+      return this.items[0];
+    }
+    average() {
+      if(!this.length){
+        throw Error ("EmptySortedList");
+      }
+      return this.sum()/this.length;
+    }
+    sum() {
+      if(!this.length){
+        return 0;
+      }
+      return this.items.reduce((total, currentValue ) => total + currentValue );
+    }
+  };
+  
+  module.exports = SortedList;
