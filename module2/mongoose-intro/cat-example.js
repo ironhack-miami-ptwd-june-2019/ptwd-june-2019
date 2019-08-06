eb2080201c30ee60652c55cb6e192d39211246ca
sandrabosk <sandraboskovic@hotmail.com>
2019-08-06 02:05:15 -0400

    add R - read

--- module2/mongoose-intro/cat-example.js
+++ module2/mongoose-intro/cat-example.js
@@ -14,7 +14,29 @@ mongoose
 // Create new cats (insert document in the DB (uses insertOne() and insertMany()))
 // ----------------------------------------------------------------------------
 // .create() is a mongoose method and under the hood uses insertOne and insertMany
+// Cat
+//     .create({ name: "Garfield", age: 17, color: "orange" })
+//     .then( catDoc => console.log("Cat create worked well: ", catDoc) )
+//     .catch( error => console.log("VCreating a new cat went wrong! Try again 😞 ", err));
+
+//     // ModelName.create().then().catch()
+
+//     const sandrasCat = new Cat({ name: "Milo", age: 4 });
+//     sandrasCat
+//         .save()
+//         .then(newCat => console.log("A created cat is: ", newCat))
+//         .catch(err => console.log("Error while creating a cat!", err));
+
+
+// Reading the cats from DB (use find(), findOne(), findById())
+// ==========================================================
+
 Cat
-    .create({ name: "Garfield", age: 17, color: "orange" })
-    .then( catDoc => console.log("Cat create worked well: ", catDoc) )
-    .catch( error => console.log("VCreating a new cat went wrong! Try again 😞 ", err));
+    .find({ age: { $gt: 10 } })
+    .then(catDoc => console.log("Found this 🐈: ", catDoc))
+    .catch( err => console.log("Error while getting the cats: ", err));
+
+Cat
+    .findById("5d48d51c036739dace0141c8")
+    .then(foundCat => console.log("Found this cat by their ID: ", foundCat))
+    .catch( err => console.log("Error while getting the cats: ", err));
