482621fe0e8dd1a1a98913df3c3f94daaed4c5c6
sandrabosk <sandraboskovic@hotmail.com>
2019-08-07 20:41:06 -0400

    cat - crud - req.params, req.query and req.body

--- module2/mongoose-intro/app.js
+++ module2/mongoose-intro/app.js
@@ -1,19 +1,92 @@
 // require all the packages you will need
 
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
 
 // package that allows templating and dynamic views
 const hbs = require("hbs");
 const PORT = 3000;
 const app = express();
 
-// import students data in app.js so we can use it in the routes
-const theData = require("./students-data");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MODELS
+const Cat = require("./models/Cat");
 
 app.set("views", __dirname + "/views");
 
 // sets hbs as default view engine
 app.set("view engine", "hbs");
+
+// CONNECT YOUR EXPRESS APP TO THE MONGO DB 
+mongoose
+                                // catApp is the name of our database
+  .connect('mongodb://localhost/catApp', {useNewUrlParser: true})
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+
+// ROUTES
+// ======================================
+// GET route - show all the cats
+app.get("/cats", (req, res, next) => {
+    console.log("IN THE ROUTE")
+    Cat.find()
+    .then(allTheCats => {
+        // console.log("cats are: ", allTheCats)
+        // res.locals makes global variable
+        res.locals.allCats = allTheCats;
+        // by default, my app knows to look for all views inside the VIEW folder
+        res.render("cats-views/cats-view");
+    })
+    .catch(err => console.log("Error while getting the cats from DB 🚨", err));
+});
+
+
+
+// GET route - show the form to user to create a new cat
+app.get("/cats/new", (req, res, next) => {
+    res.render("cats-views/new-cat");
+});
+
+// POST route - to save the cat in the DB
+app.post("/cats/create", (req, res, next) => {
+    // console.log("form data: ", req.body);
+    const newCat = {
+        // <input type="text" name="theName" id="cat-name">
+        name: req.body.theName,
+        // <input type="number" name="theAge" id="cat-age">
+        age: req.body.theAge
+    };
+    Cat.create(newCat)
+    .then(cat => {
+        // console.log("New cat created: ", cat)
+        res.redirect("/cats");
+    })
+    .catch(err => console.log("Error while creating a new cat: ", err));
+});
+
+// GET route - show details of a specific cat (req.params)
+
+app.get("/cats/:theId", (req, res, next) => {
+    // console.log("ID is: ", req.params);
+    Cat.findById(req.params.theId)
+    .then(cat => res.render("cats-views/cat-details", cat))
+    .catch(err => console.log("Err while getting details: ", err));
+});
+
+// GET route - search field (req.query)
+app.get("/search", (req, res, next) => {
+    // console.log("we typed in search this: ", req.query);
+            // "name" comes from Cat model
+                            // "catName" comes from the <input name="catName"> from the hbs file
+    Cat.find({ name: req.query.catName })
+    .then(cats => res.render("cats-views/cats-view", {cats}))
+    .catch(err => console.log("Error while getting cats in search", err));
+});
+
 
 
 app.listen(PORT, () => console.log("🏃‍ on 3000"));
--- module2/mongoose-intro/cat-example.js
+++ module2/mongoose-intro/cat-example.js
@@ -14,10 +14,10 @@ mongoose
 // Create new cats (insert document in the DB (uses insertOne() and insertMany()))
 // ----------------------------------------------------------------------------
 // .create() is a mongoose method and under the hood uses insertOne and insertMany
-// Cat
-//     .create({ name: "Garfield", age: 17, color: "orange" })
-//     .then( catDoc => console.log("Cat create worked well: ", catDoc) )
-//     .catch( error => console.log("VCreating a new cat went wrong! Try again 😞 ", err));
+Cat
+    .create({ name: "Garfield", age: 13, color: "orange" })
+    .then( catDoc => console.log("Cat create worked well: ", catDoc) )
+    .catch( error => console.log("VCreating a new cat went wrong! Try again 😞 ", err));
 
 //     // ModelName.create().then().catch()
 
@@ -31,12 +31,46 @@ mongoose
 // Reading the cats from DB (use find(), findOne(), findById())
 // ==========================================================
 
+// .find() we are always getting array back as a response
 Cat
     .find({ age: { $gt: 10 } })
     .then(catDoc => console.log("Found this 🐈: ", catDoc))
     .catch( err => console.log("Error while getting the cats: ", err));
 
+// .findById() we are always getting object back as a response
 Cat
     .findById("5d48d51c036739dace0141c8")
     .then(foundCat => console.log("Found this cat by their ID: ", foundCat))
     .catch( err => console.log("Error while getting the cats: ", err));
+
+
+// Updating cats in the DB
+// ===========================================================
+
+Cat
+    .findByIdAndUpdate("5d48d51c036739dace0141c8", { $set: { name: "Sandra", age: 17 } })
+    .then(updatedCat => console.log("Updated cat: ", updatedCat))
+    .catch( err => console.log("Error while updating the cat: ", err));
+
+    Cat                                     // age = age + 1
+        .updateMany({ name: "Garfield" }, { $inc: { age: 1 } })
+        .then(updatedCats => console.log("Updated cats: ", updatedCats))
+        .catch( err => console.log("Error while updating cats: ", err));
+
+
+// Deleting cats from DB
+// ============================================================
+
+// Cat 
+// // .findByIdAndRemove() works the same as .findByIdAndDelete()
+//     .findByIdAndDelete("5d48d2676ca362daa1ced681")
+//     .then(deletedCat => console.log(`Deleted cat with id: ${deletedCat._id}`))
+//     .catch( err => console.log("Error while deleting one cat: ", err));
+
+// Cat
+//     .deleteMany({ name: "Garfield" })
+//     .then(deletedCats => {
+//         console.log("deleted: ", deletedCats);
+//         // deletedCats.forEach(oneCat =>  console.log(`Deleted cat with id: ${oneCat._id}`));
+//     })
+//     .catch( err => console.log("Error while deleting one cat: ", err));
