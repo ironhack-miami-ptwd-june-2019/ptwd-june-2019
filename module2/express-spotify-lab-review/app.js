112b14bc0ad191d8ca13e31c0b80afc59b6c9a0b
sandrabosk <sandraboskovic@hotmail.com>
2019-08-11 10:08:05 -0400

    add spotify solution

--- module2/express-spotify-lab-review/app.js
+++ module2/express-spotify-lab-review/app.js
@@ -32,33 +32,31 @@ var spotifyApi = new SpotifyWebApi({
 });
 
 // Retrieve an access token.
-spotifyApi.clientCredentialsGrant()
+spotifyApi
+  .clientCredentialsGrant()
   .then(data => {
     // console.log("data is: ", data)
     spotifyApi.setAccessToken(data.body['access_token']);
-})
-.catch( err => {
-    console.log('Something went wrong when retrieving an access token', err);
-})
+  })
+  .catch( err => console.log('Something went wrong when retrieving an access token', err));
 
 
 // Our routes go here:
 
-app.get('/', (req, res, next) => {
-    res.render('home');
-});
+app.get('/', (req, res, next) => res.render('home'));
 
 app.get('/artists', (req, res, next) => {
 
     // console.log("req ======= ", req.query);
     // Search artists whose name contains any search term
-    spotifyApi.searchArtists(req.query.theArtistName) // <----- theArtistName is name="theArtistName" in our search form
-    .then(data => {
-    // console.log('Search artists by: ====', data.body.artists.items);
-        res.render('artists', { allTheArtists: data.body.artists.items })
-    })
-    .catch(err => console.log("Error while getting the artists: ", err));
-})
+    spotifyApi
+      .searchArtists(req.query.theArtistName) // <----- theArtistName is name="theArtistName" in our search form
+      .then(data => {
+      // console.log('Search artists by: ====', data.body.artists.items);
+        res.render('artists', { allTheArtists: data.body.artists.items });
+      })
+      .catch(err => console.log("Error while getting the artists: ", err));
+});
 
 
 //                artistId => this is placeholder, can be any word, 
@@ -69,23 +67,24 @@ app.get('/albums/:artistId', (req, res, next) => {
     //               '/albums/:artistId', the second param is optional
     //                            |
     //                            V
-  .getArtistAlbums(req.params.artistId, { limit: 5 })
-  .then(data => {
-    // console.log(" = = == = == = = = =",data.body);
-    res.render('albums', { allTheAlbums: data.body.items })
-  })
-  .catch(err => console.log("Error while getting the albums: ", err))
+    .getArtistAlbums(req.params.artistId, { limit: 5 })
+    .then(data => {
+      // console.log(" = = == = == = = = =",data.body);
+      res.render('albums', { allTheAlbums: data.body.items });
+    })
+    .catch(err => console.log("Error while getting the albums: ", err));
 })
 
 
 // Get tracks in an album
 app.get('/tracks/:albumId', (req, res, next) => { 
-spotifyApi.getAlbumTracks(req.params.albumId)
-.then(data => {
-    //   console.log("++++++++", data.body);
-    res.render('tracks', { tracks: data.body.items })
-})
-.catch(err => console.log("Error while getting the tracks: ", err));
-})
+  spotifyApi
+    .getAlbumTracks(req.params.albumId)
+    .then(data => {
+      //   console.log("++++++++", data.body);
+      res.render('tracks', { tracks: data.body.items });
+    })
+    .catch(err => console.log("Error while getting the tracks: ", err));
+});
 
 app.listen(3000);
