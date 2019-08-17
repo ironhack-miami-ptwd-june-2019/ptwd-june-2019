7f0b5baf73b4b79c73ae5d5b75406a54551e274d
nickborbe <nickborbe@gmail.com>
2019-08-17 14:07:03 -0400

    updated logout route

--- module2/movie-celeb/routes/user-routes.js
+++ module2/movie-celeb/routes/user-routes.js
@@ -86,7 +86,7 @@ router.post('/login', passport.authenticate("local", {
 
 
 router.post('/logout', (req, res, next)=>{
-  req.session.destroy()
+  req.logout();
   res.redirect('/celebrities')
 })
 
