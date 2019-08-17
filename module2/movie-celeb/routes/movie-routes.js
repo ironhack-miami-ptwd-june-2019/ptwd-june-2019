09b972a75dbd6b28188fab083cdaa714fb2cbfca
nickborbe <nickborbe@gmail.com>
2019-08-17 14:15:09 -0400

    routes file

---
+++ module2/movie-celeb/routes/movie-routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+const Celebrity = require('../models/Celebrity');
+const Movie     = require('../models/Movie')
+
+
+
+
+
+
+
+
+module.exports = router;
