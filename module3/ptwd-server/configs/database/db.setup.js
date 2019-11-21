ff4f739ac3ea1a0a40f22f2ea211c7fcb043837e
sandrabosk <sandraboskovic@hotmail.com>
2019-11-20 19:26:24 -0500

    add react cheat sheet

--- module3/ptwd-server/configs/database/db.setup.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect('mongodb://localhost/ptwd-server', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
